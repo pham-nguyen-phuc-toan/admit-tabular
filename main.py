b610f3110e8dfe16ce5777be352b4d3f282be80d
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-14 15:28:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import numpy as np
 
 IMG_SIZE = 227
 
-class_list = {'0': 'NORMAL', '1': 'PNEUMONIA'}
-
 st.title('Pneumonia prediction based on chest X-Ray image')
 
 input = open('lrc_xray.pkl', 'rb')
@@ -15,6 +13,14 @@ model = pkl.load(input)
 st.header('Upload chest X-Ray image')
 uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
 
+gre = st.number_input('Insert GRE Score')
+toefl = st.number_input('Insert TOEFL Score')
+uni_rate = st.number_input('Insert University Rating')
+sop = st.number_input('Insert SOP')
+lor = st.number_input('Insert LOR')
+cgpa = st.number_input('Insert CGPA')
+research = st.number_input('Insert Research')
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Test image')
@@ -22,7 +28,7 @@ if uploaded_file is not None:
     if st.button('Predict'):
         image = image.resize((IMG_SIZE*IMG_SIZE*3, 1))
         feature_vector = np.array(image)
-        label = str((model.predict(feature_vector))[0])
+        result = str((model.predict(feature_vector))[0])
 
         st.header('Result')
-        st.text(class_list[label])
+        st.text(result)
